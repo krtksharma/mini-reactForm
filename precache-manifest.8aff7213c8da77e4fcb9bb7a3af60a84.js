efd6a63496c73a62ffe879c6de028cae06b3e1d5
krtksharma <krtksharma7@gmail.com>
2021-11-11 11:31:49 +0530

    Updates

---
+++ precache-manifest.8aff7213c8da77e4fcb9bb7a3af60a84.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "481a5e9cce7ed704cf727a47f8ac5a9c",
+    "url": "/mini-reactForm/index.html"
+  },
+  {
+    "revision": "82efe071950e9373f227",
+    "url": "/mini-reactForm/static/js/2.710efbe2.chunk.js"
+  },
+  {
+    "revision": "21a87488d271c54f4719",
+    "url": "/mini-reactForm/static/js/main.40360135.chunk.js"
+  },
+  {
+    "revision": "d872c517661b2ca6d7f0",
+    "url": "/mini-reactForm/static/js/runtime-main.e3ea0c79.js"
+  }
+]);
